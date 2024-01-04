2e2fa29e45f462c41ff49ef8cb25775ac3bb0a38
unknownghostz6 <88034715+unknownghostz6@users.noreply.github.com>
2024-01-03 21:48:13 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,10 @@ game 'rdr3'
 fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 ui_page "ui/index.html"
+
+description 'rsg-mdt'
 version '1.1'
+author 'Unknown Ghostz'
 
 files {
     "ui/index.html",
