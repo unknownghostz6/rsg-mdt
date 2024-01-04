f8b3ff768f5bd3ee95261243673402df1efe28e3
unknownghostz6 <88034715+unknownghostz6@users.noreply.github.com>
2024-01-03 21:35:53 -0700

    added new version checker

--- sv_mdt.lua
+++ sv_mdt.lua
@@ -1,5 +1,35 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
+-----------------------------------------------------------------------
+-- version checker
+-----------------------------------------------------------------------
+local function versionCheckPrint(_type, log)
+    local color = _type == 'success' and '^2' or '^1'
+
+    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
+end
+
+local function CheckVersion()
+    PerformHttpRequest('https://raw.githubusercontent.com/unknownghostz6/rsg-mdt/main/version.txt', function(err, text, headers)
+        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+
+        if not text then 
+            versionCheckPrint('error', 'Currently unable to run a version check.')
+            return 
+        end
+
+      
+        if text == currentVersion then
+            versionCheckPrint('success', 'You are running the latest version.')
+        else
+            versionCheckPrint('error', ('Current Version: %s'):format(currentVersion))
+            versionCheckPrint('success', ('Latest Version: %s'):format(text))
+            versionCheckPrint('error', ('You are currently running an outdated version, please update to version %s'):format(text))
+        end
+    end)
+end
+
+
 RegisterCommand(""..Config.Command.."", function(source, args)
     local _source = source
 	local tablex = {}
