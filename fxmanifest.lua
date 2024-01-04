3478e16954cdabeb6007cb56ca6d046fa42f8cef
unknownghostz6 <88034715+unknownghostz6@users.noreply.github.com>
2024-01-03 21:53:37 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,8 @@ fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 ui_page "ui/index.html"
 
+version '1.1'
+
 files {
     "ui/index.html",
     "ui/vue.min.js",
