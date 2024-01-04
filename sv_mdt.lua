e8f728f3ed6db2a23aa74c6d03068dab0d1dc41d
unknownghostz6 <88034715+unknownghostz6@users.noreply.github.com>
2024-01-03 21:38:00 -0700

    added new version checker

--- sv_mdt.lua
+++ sv_mdt.lua
@@ -10,7 +10,7 @@ local function versionCheckPrint(_type, log)
 end
 
 local function CheckVersion()
-    PerformHttpRequest('https://raw.githubusercontent.com/unknownghostz6/rsg-mdt/main/version.txt', function(err, text, headers)
+    PerformHttpRequest('https://github.com/unknownghostz6/rsg-mdt/blob/main/version.txt', function(err, text, headers)
         local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
 
         if not text then 
